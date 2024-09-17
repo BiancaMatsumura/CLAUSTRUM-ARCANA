a6f536b5e004a093268af424c2184836ffd516b4
Bianca Dellevedove Matsumura <bdmatsumura@sga.pucminas.br>
2024-09-16 22:18:44 -0300

    rotação inicial do objeto / UI de movimentação

--- CLAUSTRUM ARCANA/Assets/1.Scripts/InteractableItem.cs
+++ CLAUSTRUM ARCANA/Assets/1.Scripts/InteractableItem.cs
@@ -1,14 +1,112 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class InteractableItem : MonoBehaviour, IInteractable
 {
+    public GameObject interactableItem;
+    public GameObject uiElements;
+    public GameObject buttonRotate;
+
+    private bool isSelected = false;
+    private bool canRotate = true;
+
+    private bool isRotating = false;
+    public float rotationSpeed = 200f;
+
+    private float targetRotationAngle = 0f; 
+    private float rotationProgress = 0f;
+
+    private static InteractableItem currentSelectedItem;
+
     public void Interact()
-    {
+       {
+        if (!isSelected)
+        {
+            SelectItem();
+        }
 
-        Debug.Log("Interagindo com o Item");
-
+       
     }
 
+    public void Deselect()  
+    {
+        if (isSelected)
+        {
+            DeselectItem();
+        }
+    }
+
+    public bool IsSelected() 
+    {
+        return isSelected;
+    }
+
+
+    private void SelectItem()
+    {
+        if (currentSelectedItem != null)
+        {
+            currentSelectedItem.Deselect(); 
+        }
+
+        isSelected = true;
+        currentSelectedItem = this;
+
+        if (uiElements != null && buttonRotate != null)
+        {
+            uiElements.SetActive(true); 
+            buttonRotate.SetActive(true);
+        }
+
+        Debug.Log("Item Selecionado");
+    }
+
+    private void DeselectItem()
+    {
+        isSelected = false;
+        currentSelectedItem = null;
+
+        if (uiElements != null && buttonRotate != null)
+        {
+            uiElements.SetActive(false);
+            buttonRotate.SetActive(false);
+        }
+
+        Debug.Log("Item Deselecionado");
+    }
+
+    public void Rotate()
+    {
+     
+        if (canRotate && !isRotating && isSelected)
+        {
+            targetRotationAngle += 90f;
+            isRotating = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isRotating)
+        {
+            
+            rotationProgress = Mathf.MoveTowards(rotationProgress, targetRotationAngle, rotationSpeed * Time.deltaTime);
+
+            
+            interactableItem.transform.rotation = Quaternion.Euler(0f, rotationProgress, 0f);
+
+            
+            if (Mathf.Abs(rotationProgress - targetRotationAngle) < 0.1f)
+            {
+                
+                interactableItem.transform.rotation = Quaternion.Euler(0f, targetRotationAngle, 0f);
+                isRotating = false; 
+                Debug.Log("Item rotacionado suavemente para " + targetRotationAngle + " graus");
+            }
+        }
+    }
 }
--- CLAUSTRUM ARCANA/Assets/1.Scripts/Interactor.cs
+++ CLAUSTRUM ARCANA/Assets/1.Scripts/Interactor.cs
@@ -5,40 +5,61 @@ using UnityEngine;
 interface IInteractable
 {
     public void Interact();
+    public void Deselect();
+    public bool IsSelected();  
 }
 
 public class Interactor : MonoBehaviour
 {
-
     public Transform InteractorSource;
     public float InteractRange;
-    
-    void Start()
-    {
-        
-    }
 
-    
+    private IInteractable currentInteractable;  
+
     void Update()
     {
-
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
+
             if (touch.phase == TouchPhase.Began)
             {
                 Ray ray = new Ray(InteractorSource.position, InteractorSource.forward);
                 if (Physics.Raycast(ray, out RaycastHit hitInfo, InteractRange))
                 {
+                   
                     if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
                     {
+                        
+                        if (currentInteractable != null && currentInteractable != interactObj)
+                        {
+                            currentInteractable.Deselect();
+                        }
+
+                        
                         interactObj.Interact();
+                        currentInteractable = interactObj;
                     }
-
+                    else
+                    {
+                        
+                        if (currentInteractable != null)
+                        {
+                            currentInteractable.Deselect();
+                            currentInteractable = null; 
+                        }
+                    }
                 }
-
+                else
+                {
+                    
+                    if (currentInteractable != null)
+                    {
+                        currentInteractable.Deselect();
+                        currentInteractable = null;
+                    }
+                }
             }
         }
-
     }
 }
