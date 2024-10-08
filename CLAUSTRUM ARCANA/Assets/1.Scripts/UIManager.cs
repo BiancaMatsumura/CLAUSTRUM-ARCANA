9a12f9f513658c68d7065f8f0351a193f527e787
Pedro Henrique de Freitas Souza <pedrohsouza33@gmail.com>
2024-10-08 13:25:03 +0100

    botao e painel do pergaminho provisorios

--- CLAUSTRUM ARCANA/Assets/1.Scripts/UIManager.cs
+++ CLAUSTRUM ARCANA/Assets/1.Scripts/UIManager.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     private InteractableItem currentItem;
-
-
     public LevelLoader levelLoader;
+
+    public GameObject panel; // Arraste o painel que você deseja abrir aqui
+    private bool isPanelOpen = false; // Estado inicial do painel
 
     public void SetCurrentItem(InteractableItem item)
     {
@@ -27,22 +28,14 @@ public class UIManager : MonoBehaviour
         levelLoader.Transition(sceneName);
     }
 
-
-
-    public void Option()
+    // Função para abrir/fechar o painel ao clicar no botão
+    public void TogglePanel()
     {
-
+        isPanelOpen = !isPanelOpen;
+        panel.SetActive(isPanelOpen);
     }
 
-    public void Credits()
-    {
-
-    }
-
-
-    public void Quit()
-    {
-        Application.Quit();
-    }
-
+    public void Option() { }
+    public void Credits() { }
+    public void Quit() { Application.Quit(); }
 }
